e5fcb7e1294eff976a970983df7d4e632696b768
XYShaoKang <zengwenjie03@gmail.com>
2020-05-26 03:00:45 +0800

    feat(gatsby-hello-world): support styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,20 @@
 import React from "react"
+import styled from "styled-components"
 
-export default () => <div>Hello world!</div>
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`
+
+// Create a Wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`
+
+export default () => (
+  <Wrapper>
+    <Title>Hello World!</Title>
+  </Wrapper>
+)
